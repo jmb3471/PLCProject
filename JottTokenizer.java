212542a3f1d8c7c0ebca5d6b5e309fda7e96ac10
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2022-09-05 22:12:25 -0400

    began adding tokenizing
    added tokenizing list, finished commas, brackets, comments, newlines, whitespaces, and braces

--- JottTokenizer.java
+++ JottTokenizer.java
@@ -1,7 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * This class is responsible for tokenizing Jott code.
  * 
- * @author 
+ * @author Jonathan Baxley
  **/
 
 import java.util.ArrayList;
@@ -15,6 +18,50 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
+		File file = new File(filename);
+		Scanner sc = new Scanner(file);
+		ArrayList<Token> tokenList = new ArrayList<Token>();
+		int i = 0;
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			int j = 0;
+			while (j < line.length()) {
+				String token = "";
+				if (line[j] == " ") {
+					j++;
+				}
+				else if (line[j] == "#") {
+					break;
+				}
+				else if (line[j] == ",") {
+					token = ",";
+					Token token1 = new Token(token, filename, i, TokenType.COMMA);
+					tokenList.add(token1);
+				}
+				else if (line[j] == "[") {
+					token = "[";
+					Token token1 = new Token(token, filename, i, TokenType.L_BRACKET);
+					tokenList.add(token1);
+				}
+				else if (line[j] == "]") {
+					token = "]";
+					Token token1 = new Token(token, filename, i, TokenType.R_BRACKET);
+					tokenList.add(token1);
+				}
+				else if (line[j] == "{") {
+					token = "{";
+					Token token1 = new Token(token, filename, i, TokenType.L_BRACE);
+					tokenList.add(token1);
+				}
+				else if (line[j] == "}") {
+					token = "}";
+					Token token1 = new Token(token, filename, i, TokenType.R_BRACE);
+					tokenList.add(token1);
+				}
+				j++;
+			}
+			i++;
+		}
 		return null;
 	}
 }
