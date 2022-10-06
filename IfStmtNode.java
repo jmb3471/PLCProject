9b1a3b28ff10a8683f4488f61b0b1ca4ec227104
sjs7128 <sjs7128@rit.edu>
2022-10-06 12:15:51 -0400

    Finished ParseIfStmtNode

--- IfStmtNode.java
+++ IfStmtNode.java
@@ -10,6 +10,12 @@ public class IfStmtNode extends BodyStmtNode implements JottTree{
 
     private String type;
 
+    private ExprNode cond;
+    private BodyNode Body;
+    private ArrayList<BodyNode> elseIfBodys;
+    private ArrayList<ExprNode> elseIfExprs;
+    private BodyNode elseBody;
+
 
     public IfStmtNode() {
         this.else_stmt = null;
@@ -19,16 +25,87 @@ public class IfStmtNode extends BodyStmtNode implements JottTree{
         this.type = "Ifstmt";
     }
 
+    public IfStmtNode(ExprNode cond, BodyNode body, ArrayList<BodyNode> elseIfBodys, ArrayList<ExprNode> elseIfExprs, BodyNode elseBody) {
+        this.cond = cond;
+        this.Body = body;
+        this.elseIfBodys = elseIfBodys;
+        this.elseIfExprs = elseIfExprs;
+        this.elseBody = elseBody;
+    }
+
     public static IfStmtNode ParseIfStmtNode(ArrayList<Token> tokens) {
-        if (tokens.get(1).getTokenType() != TokenType.L_BRACKET) {
+        if (tokens.get(0).getTokenType() != TokenType.L_BRACKET) {
             return null;
         }
 
         tokens.remove(0);
 
-        BExprNode bExpr= BExprNode.ParseBExprNode(tokens);
+        ExprNode expr = ExprNode.ParseExprNode(tokens);
 
-        return null;
+        if (tokens.get(0).getTokenType() != TokenType.R_BRACKET) {
+            return null;
+        }
+
+        if (tokens.get(1).getTokenType() != TokenType.L_BRACE) {
+            return null;
+        }
+
+        tokens.remove(1);
+        tokens.remove(0);
+
+        BodyNode body = BodyNode.ParseBodyNode(tokens);
+
+        if (tokens.get(0).getTokenType() != TokenType.R_BRACE) {
+            return null;
+        }
+
+        ArrayList<BodyNode> elseIFBodys = new ArrayList<>();
+        ArrayList<ExprNode> elseIfExprs = new ArrayList<>();
+
+        BodyNode elseNode = null;
+
+        while (tokens.get(0).getToken() == "elseif") {
+            if (tokens.get(1).getTokenType() != TokenType.L_BRACKET) {
+                return null;
+            }
+
+            tokens.remove(1);
+            tokens.remove(0);
+
+            ExprNode elseIfExpr = ExprNode.ParseExprNode(tokens);
+
+            if (tokens.get(0).getTokenType() != TokenType.R_BRACKET) {
+                return null;
+            }
+
+            if (tokens.get(1).getTokenType() != TokenType.L_BRACE) {
+                return null;
+            }
+
+            tokens.remove(1);
+            tokens.remove(0);
+
+            BodyNode elseIfBody = BodyNode.ParseBodyNode(tokens);
+
+            if (tokens.get(0).getTokenType() != TokenType.R_BRACE) {
+                return null;
+            }
+
+            tokens.remove(0);
+
+            elseIFBodys.add(elseIfBody);
+            elseIfExprs.add(elseIfExpr);
+
+
+        }
+
+        if (tokens.get(0).getToken() == "else") {
+            elseNode = BodyNode.ParseBodyNode(tokens);
+        }
+
+        IfStmtNode ifNode = new IfStmtNode(expr, body, elseIFBodys, elseIfExprs, body);
+
+        return ifNode;
     }
 
     @Override
