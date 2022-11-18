b583c57b1c41175c7724ff128455b27b8674c935
sjs7128 <sjs7128@rit.edu>
2022-11-17 20:26:14 -0500

    Jott.java now runs as expected

--- Jott.java
+++ Jott.java
@@ -1,47 +1,82 @@
+import java.io.Console;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Jott {
     public static void main(String[] args) {
-
-        // don't we accept 4 command line args? 
-        //if (args.length != 3) 
-        //{
-        //    return;
-        //}
+        if (args.length != 3) 
+        {
+            System.out.println("Usage for Jott Translator:\n" +
+                                "\tJott:\t java Jott <input.jott> <output.jott> Jott\n" +
+                                "\tPyhon 3: java Jott <input.jott> <output.py> Python\n" +
+                                "\tJava:\t java Jott <input.jott> <output.java> Java\n" +
+                                "\tC:\t java Jott <input.jott> <output.c> C");
+            return;
+        }
 
         try 
         {
-            //System.out.println("args[0] = " + args[0]);
-            //System.out.println("args[1] = " + args[1]);
-            //System.out.println("args[2] = " + args[2]);
-            //System.out.println("args[3] = " + args[3]);
-
-            System.out.println("Tokenizing...");
+            System.out.println("Tokenizing...\n");
             ArrayList<Token> tokens = JottTokenizer.tokenize(args[0]);
-            System.out.println("Parsing...");
-            JottTree tree = ProgramNode.ParseProgram(tokens);
-            boolean valid = false;
-
-            if (tree != null) {
-                System.out.println("Validating...");
-                valid = tree.validateTree();
-            }
-
-            if (!valid) {
-                System.out.println("Invalid Tree");
+            if (tokens.isEmpty()){
+                System.out.println(args[0] + " failed while tokenizing.");
                 return;
             }
 
-            System.out.println("Valid Tree");
-
-            String output = "";
-            if (args[2].equals("Jott") && args[1].contains(".jott")) {
-                output = tree.convertToJott();
+            System.out.println("Parsing...\n");
+            JottTree tree = ProgramNode.ParseProgram(tokens);
+            if (tree == null){
+                System.out.println(args[0] + " failed while parsing.");
+                return;
             }
+            
+            System.out.println("Validating...\n");
+            boolean valid = tree.validateTree();
+
+            if (!valid) {
+                System.out.println(args[0] + " failed while validating.");
+                return;
+            }
+
+            System.out.println("Valid Tree\n");
+
+            String output;
+            switch (args[2]) {
+                case "Jott":
+                    System.out.println("Converting to Jott...\n");
+                    output = tree.convertToJott();
+                    break;
+
+                case "Python":
+                    System.out.println("Converting to Python...\n");
+                    output = tree.convertToPython();
+                    break;
+
+                case "Java":
+                    System.out.println("Converting to Java...\n");
+                    output = tree.convertToJava();
+                    break;
+
+                case "C":
+                    System.out.println("Converting to C...\n");
+                    output = tree.convertToC();
+                    break;
+
+                default:
+                    System.out.println(args[2] + " is not a valid language to convert to.");
+                    return;
+            }
+
+            if (output == null){
+                System.out.println("Error occured while converting");
+                return;
+            }
+
             FileWriter fileWriter = new FileWriter(args[1]);
             fileWriter.write(output);
             fileWriter.close();
+
+            System.out.println("Output file saved.");
 
         } catch (Exception e) {
             e.printStackTrace();
